a6a8e4b3cf470927a349c8618390011960bee6ca
FelippeRoza <felippesroza@gmail.com>
2017-12-07 16:05:59 -0200

    some improvements in plotting data

--- tests/arduino-matlab/plot.py
+++ tests/arduino-matlab/plot.py
@@ -1,35 +1,60 @@
 import pandas as pd
 from pylab import *
+import re
 
-folder_name = 'testes_07_11/'
-file_name = 'vazio_pitch0.txt'
+folder_name = 'test_2017_12_06/'
+# file_name_list = ['24v_pitch05_v', '24v_pitch15_v', '24v_pitch25_v', '24v_pitch35_v']
+file_name_list =  ['vazio_pitch05_v', 'vazio_pitch15_v', 'vazio_pitch25_v', 'vazio_pitch35_v']
 
 def convert_voltage(series):
     return series*5*110/(1024*10)
 
 def convert_current(series):
-    return (series*(5/1024)-2.5)*0.086
+    return series/260
 
-df = pd.read_csv(folder_name+file_name, sep = '\t', skiprows = 1, header = None)
-df.columns = ['v1', 'i1', 'v2', 'i2', 'v3', 'i3', 'rpm', 'windspeed', 'time']
-for col in df.columns:
-    if col[0] == 'v':
-        df[col] = convert_voltage(df[col])
-    if col[0] == 'i':
-        df[col] = convert_current(df[col])
+def plot_wind(files_list):
+    ax = {}
+    fig = {}
+    print 'frango'
+    fig = plt.figure()
 
-df['rpm'] = df['rpm']/1000 #to fit in scale
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-ax1.plot(df['time'], df['rpm'])
-ax1.plot(df['time'], df['v3'])
-legend = legend(["RPM", "V_gen"], loc=2);
-# fig2 = plt.figure()
-# ax2 = fig2.add_subplot(111)
-# ax2.plot(df['time'], df['windspeed'])
-#
-# fig3 = plt.figure()
-# ax3 = fig3.add_subplot(111)
-# ax3.plot(df['time'], df['v3'])
+    for file_name in files_list:
+        last_time = 0.0
+        df = pd.DataFrame()
 
-show()
+        for i in range(1, 5):
+            filename = folder_name+file_name+str(i)+'.txt'
+            if df.empty:
+                df = pd.read_csv(filename, sep = '\t', skiprows = 1, header = None)
+                df.columns = ['v1', 'i1', 'v2', 'i2', 'v3', 'i3', 'rpm', 'windspeed', 'time']
+                df = df[df['time'] < 20.0]
+            else:
+                temp = pd.read_csv(filename, sep = '\t', skiprows = 1, header = None)
+                temp.columns = ['v1', 'i1', 'v2', 'i2', 'v3', 'i3', 'rpm', 'windspeed', 'time']
+                temp = temp[temp['time'] < 20.0]
+                temp['time'] = temp['time'] + last_time
+                df = df.append(temp)
+
+            last_time = float(df.tail(1)['time'])
+        df = df[df['rpm'] > df['rpm'].median()/1.5]
+
+        for col in df.columns:
+            if col[0] == 'v':
+                df[col] = convert_voltage(df[col])
+            if col[0] == 'i':
+                df[col] = convert_current(df[col])
+        pitch = 'Pitch = ' + re.search('pitch(.+?)_', file_name).group(1)
+
+        # df['rpm'] = df['rpm']/1000 #to fit in scale
+        ax[file_name] = fig.add_subplot(111)
+        ax[file_name].plot(df['time'], df['rpm'], label = pitch)
+
+        xlabel('t(s)')
+        ylabel(r'$\omega$ (RPM)')
+        title('Open circuit')
+        # ax[file_name].plot(df['time'], df['v3'], label= 'V_gen')
+
+    legend(loc='upper left')
+    show()
+
+plot_wind(file_name_list)
