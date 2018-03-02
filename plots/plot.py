766dfbb1ae6eedb8f7bee13412b917cbe7a4c071
FelippeRoza <felippesroza@gmail.com>
2018-03-02 15:39:25 -0300

    updated plot script and figures

--- plots/plot.py
+++ plots/plot.py
@@ -8,14 +8,22 @@ def plot_simple_turbine():
     for i in range(1,7):
         df = pd.read_csv(folder + str(i) + '.csv')
         df[df < 0]  = 0
-        plt.plot(df['Vo'], df['Pe'])
+        label = str(int(df.head(1)['S2.v_ms'])) + ' m/s'
+        plt.plot(df['Vo'], df['Pe'], label = label, linewidth=2)
 
-    x = np.arange(0.0, 40.0, 0.1)
-    y = 0.0067 * (x**3) # equation fitted in matlab by least squares
-    plt.plot(x, y)
+    # fig.suptitle('test title')
+    plt.xlabel('Vo (V)')
+    plt.ylabel('Po (W)')
+    plt.legend(loc="upper left")
     axes = plt.gca()
     axes.set_ylim([0,150])
     plt.savefig(out_folder + 'simple-turbine-power-x-voltage.png')
+
+    x = np.arange(0.0, 40.0, 0.1)
+    y = 0.0067 * (x**3) # equation fitted in matlab by least squares
+    plt.plot(x, y, label = 'MPP')
+    plt.legend(loc="upper left")
+    plt.savefig(out_folder + 'simple-turbine-cubic-fitted.png')
 
 
 plot_simple_turbine()
